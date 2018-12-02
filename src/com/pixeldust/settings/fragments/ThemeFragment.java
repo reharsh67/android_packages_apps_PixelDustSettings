b740435a833bc2b4e9601900b04aa3d423859711
Kshitij Gupta <kshitijgm@gmail.com>
2018-12-02 23:57:08 +0530

    themes: Saucy pebbles
    - Inspired by DU's Fruity Pebbles but is a complete rewrite with no
      hardcoded package names and/or colors. This will find all themes on
      the device categorised by "android.theme" in OverlayInfo and display
      a preview by reading the material 500 shade of the color from the
      overlay package in question.
    - This requires no further dependencies. Do note, this will list only
      overlays categorised by "android.theme" and the accent packages need
      to be updated accordingly. See <INSERT COMMIT HERE>.
    
    Change-Id: Ic141086e37f501dea5158af2275c11e8deb7fa5c
    Signed-off-by: Kshitij Gupta <kshitijgm@gmail.com>
    
    spezi77 edits: Add a main category for Themes
    Signed-off-by: spezi77 <spezi7713@gmx.net>

---
+++ src/com/pixeldust/settings/fragments/ThemeFragment.java
@@ -0,0 +1,145 @@
+/*
+ * Copyright (C) 2018 The Potato Open Sauce Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.pixeldust.settings.fragments;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.os.ServiceManager;
+import android.os.UserHandle;
+import android.content.Context;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
+import android.provider.SearchIndexableResource;
+import android.support.v7.preference.Preference;
+import android.support.v7.preference.PreferenceCategory;
+import android.support.v7.preference.PreferenceScreen;
+import android.support.v14.preference.PreferenceFragment;
+
+import com.android.settings.R;
+import com.android.internal.logging.nano.MetricsProto;
+import com.android.settings.search.BaseSearchIndexProvider;
+import com.android.settings.search.Indexable;
+import com.android.settings.SettingsPreferenceFragment;
+import com.android.settings.wrapper.OverlayManagerWrapper;
+import com.android.settings.wrapper.OverlayManagerWrapper.OverlayInfo;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ThemeFragment extends SettingsPreferenceFragment
+        implements Preference.OnPreferenceChangeListener, Indexable {
+
+    private static final String KEY_ACCENT_PICKER = "accent_picker";
+    private Preference mSystemThemeColor;
+    private Fragment mCurrentFragment = this;
+    private OverlayManagerWrapper mOverlayService;
+    private PackageManager mPackageManager;
+
+    @Override
+    public boolean onPreferenceChange(Preference preference, Object newValue) {
+        return false;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        addPreferencesFromResource(R.xml.pixeldust_settings_themes);
+        mSystemThemeColor = (Preference) findPreference(KEY_ACCENT_PICKER);
+
+        // OMS and PMS setup
+        mOverlayService = ServiceManager.getService(Context.OVERLAY_SERVICE) != null ? new OverlayManagerWrapper()
+                : null;
+        mPackageManager = getActivity().getPackageManager();
+        String currentPkg = getTheme();
+        CharSequence label = null;
+        try {
+            label = mPackageManager.getApplicationInfo(currentPkg, 0).loadLabel(mPackageManager);
+        } catch (PackageManager.NameNotFoundException e) {
+            label = currentPkg;
+        }
+        mSystemThemeColor.setSummary(label);
+    }
+
+    public void updateEnableState() {
+        if (mSystemThemeColor == null) {
+            return;
+        }
+        mSystemThemeColor.setOnPreferenceClickListener(new Preference.OnPreferenceClickListener() {
+            @Override
+            public boolean onPreferenceClick(Preference preference) {
+                AccentPicker.show(mCurrentFragment, preference);
+                return true;
+            }
+        });
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        updateEnableState();
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+    }
+
+    public static final SearchIndexProvider SEARCH_INDEX_DATA_PROVIDER = new BaseSearchIndexProvider() {
+
+        @Override
+        public List<SearchIndexableResource> getXmlResourcesToIndex(Context context, boolean enabled) {
+            List<SearchIndexableResource> indexables = new ArrayList<>();
+            SearchIndexableResource indexable = new SearchIndexableResource(context);
+            indexable.xmlResId = R.xml.pixeldust_settings_themes;
+            indexables.add(indexable);
+            return indexables;
+        }
+
+        @Override
+        public List<String> getNonIndexableKeys(Context context) {
+            List<String> keys = super.getNonIndexableKeys(context);
+            return keys;
+        }
+    };
+
+    @Override
+    public int getMetricsCategory() {
+        return MetricsProto.MetricsEvent.PIXELDUST;
+    }
+
+    private String getTheme() {
+        List<OverlayInfo> infos = mOverlayService.getOverlayInfosForTarget("android", UserHandle.myUserId());
+        for (int i = 0, size = infos.size(); i < size; i++) {
+            if (infos.get(i).isEnabled() && isTheme(infos.get(i))) {
+                return infos.get(i).packageName;
+            }
+        }
+        return null;
+    }
+
+    private boolean isTheme(OverlayInfo oi) {
+        if (!OverlayInfo.CATEGORY_THEME.equals(oi.category)) {
+            return false;
+        }
+        try {
+            PackageInfo pi = mPackageManager.getPackageInfo(oi.packageName, 0);
+            return pi != null && !pi.isStaticOverlayPackage();
+        } catch (PackageManager.NameNotFoundException e) {
+            return false;
+        }
+    }
+}

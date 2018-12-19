5d619a8fc6eb7e138f760aa5999e87a001a73a35
Pranav Vashi <neobuddy89@gmail.com>
2018-12-19 11:05:53 +0530

    Add config to enable face auto unlock by default [2/2]
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    Signed-off-by: spezi77 <spezi7713@gmx.net>

--- src/com/pixeldust/settings/fragments/LockScreenSettings.java
+++ src/com/pixeldust/settings/fragments/LockScreenSettings.java
@@ -1,19 +1,47 @@
 package com.pixeldust.settings.fragments;
 
-import com.android.internal.logging.nano.MetricsProto;
-
+import android.content.ContentResolver;
+import android.content.Context;
+import android.content.res.Resources;
+import android.net.Uri;
 import android.os.Bundle;
-import com.android.settings.R;
+import android.os.UserHandle;
+import android.provider.Settings;
+import android.support.v14.preference.SwitchPreference;
+import android.support.v7.preference.Preference;
+import android.support.v7.preference.PreferenceScreen;
 
+import com.android.internal.logging.nano.MetricsProto;
+import com.android.internal.util.pixeldust.PixeldustUtils;
+import com.android.settings.R;
 import com.android.settings.SettingsPreferenceFragment;
 
 public class LockScreenSettings extends SettingsPreferenceFragment {
+
+    private static final String FACE_UNLOCK_PREF = "face_auto_unlock";
+    private static final String FACE_UNLOCK_PACKAGE = "com.android.facelock";
+
+    private SwitchPreference mFaceUnlock;
 
     @Override
     public void onCreate(Bundle icicle) {
         super.onCreate(icicle);
 
         addPreferencesFromResource(R.xml.pixeldust_settings_lockscreen);
+
+        boolean mFaceUnlockEnabled = Settings.Secure.getIntForUser(getActivity().getContentResolver(),
+                Settings.Secure.FACE_AUTO_UNLOCK, getActivity().getResources().getBoolean(
+                com.android.internal.R.bool.config_face_unlock_enabled_by_default) ? 1 : 0,
+                UserHandle.USER_CURRENT) != 0;
+
+        mFaceUnlock = (SwitchPreference) findPreference(FACE_UNLOCK_PREF);
+        mFaceUnlock.setChecked(mFaceUnlockEnabled);
+
+        if (!PixeldustUtils.isPackageInstalled(getActivity(), FACE_UNLOCK_PACKAGE)) {
+            mFaceUnlock.setEnabled(false);
+            mFaceUnlock.setSummary(getActivity().getString(
+                    R.string.face_auto_unlock_not_available));
+        }
     }
 
     @Override

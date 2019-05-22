66bb7eb1c00a944a77a6baad50518981e6802a75
spezi77 <spezi7713@gmx.net>
2019-05-22 21:35:12 +0200

    QS tile styles: Allow to preview/pick more fancy styles.
    Signed-off-by: spezi77 <spezi7713@gmx.net>

--- src/com/pixeldust/settings/fragments/QsTileStyles.java
+++ src/com/pixeldust/settings/fragments/QsTileStyles.java
@@ -1,5 +1,6 @@
 /*
  * Copyright (C) 2018 The Dirty Unicorns Project
+ * Copyright (C) 2019 The PixelDust Project
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -90,6 +91,49 @@ public class QsTileStyles extends InstrumentedDialogFragment implements OnClickL
 
         LinearLayout teardrop = mView.findViewById(R.id.QsTileStyleTearDrop);
         setLayout("4", teardrop);
+
+        LinearLayout circlegradient = mView.findViewById(R.id.QsTileStyleCirclegradient);
+        setLayout("5", circlegradient);
+
+        LinearLayout circletrim = mView.findViewById(R.id.QsTileStyleCircletrim);
+        setLayout("6", circletrim);
+
+        LinearLayout dottedcircle = mView.findViewById(R.id.QsTileStyleDottedcircle);
+        setLayout("7", dottedcircle);
+
+        LinearLayout dualtonecircle = mView.findViewById(R.id.QsTileStyleDualtonecircle);
+        setLayout("8", dualtonecircle);
+
+        LinearLayout dualtonecircletrim = mView.findViewById(R.id.QsTileStyleDualtonecircletrim);
+        setLayout("9", dualtonecircletrim);
+
+        LinearLayout mountain = mView.findViewById(R.id.QsTileStyleMountain);
+        setLayout("10", mountain);
+
+        LinearLayout ninja = mView.findViewById(R.id.QsTileStyleNinja);
+        setLayout("11", ninja);
+
+        LinearLayout pokesign = mView.findViewById(R.id.QsTileStylePokesign);
+        setLayout("12", pokesign);
+
+        LinearLayout wavey = mView.findViewById(R.id.QsTileStyleWavey);
+        setLayout("13", wavey);
+
+        LinearLayout squircletrim = mView.findViewById(R.id.QsTileStyleSquircletrim);
+        setLayout("14", squircletrim);
+
+        LinearLayout cookie = mView.findViewById(R.id.QsTileStyleCookie);
+        setLayout("15", cookie);
+
+        LinearLayout oreo = mView.findViewById(R.id.QsTileStyleOreo);
+        setLayout("16", oreo);
+
+        LinearLayout oreocircletrim = mView.findViewById(R.id.QsTileStyleCircletrimOreo);
+        setLayout("17", oreocircletrim);
+
+        LinearLayout oreosquircletrim = mView.findViewById(R.id.QsTileStyleSquircletrimOreo);
+        setLayout("18", oreosquircletrim);
+
     }
 
     @Override
@@ -138,36 +182,138 @@ public class QsTileStyles extends InstrumentedDialogFragment implements OnClickL
         LinearLayout roundedsquare = mView.findViewById(R.id.QsTileStyleRoundedSquare);
         LinearLayout squircle = mView.findViewById(R.id.QsTileStyleSquircle);
         LinearLayout teardrop = mView.findViewById(R.id.QsTileStyleTearDrop);
-
-        TypedValue typedValue = new TypedValue();
-        res.getValue(R.dimen.qs_styles_layout_opacity, typedValue, true);
-        float mLayoutOpacity = typedValue.getFloat();
+        LinearLayout circlegradient = mView.findViewById(R.id.QsTileStyleCirclegradient);
+        LinearLayout circletrim = mView.findViewById(R.id.QsTileStyleCircletrim);
+        LinearLayout dottedcircle = mView.findViewById(R.id.QsTileStyleDottedcircle);
+        LinearLayout dualtonecircle = mView.findViewById(R.id.QsTileStyleDualtonecircle);
+        LinearLayout dualtonecircletrim = mView.findViewById(R.id.QsTileStyleDualtonecircletrim);
+        LinearLayout mountain = mView.findViewById(R.id.QsTileStyleMountain);
+        LinearLayout ninja = mView.findViewById(R.id.QsTileStyleNinja);
+        LinearLayout pokesign = mView.findViewById(R.id.QsTileStylePokesign);
+        LinearLayout wavey = mView.findViewById(R.id.QsTileStyleWavey);
+        LinearLayout squircletrim = mView.findViewById(R.id.QsTileStyleSquircletrim);
+        LinearLayout cookie = mView.findViewById(R.id.QsTileStyleCookie);
+        LinearLayout oreo = mView.findViewById(R.id.QsTileStyleOreo);
+        LinearLayout oreocircletrim = mView.findViewById(R.id.QsTileStyleCircletrimOreo);
+        LinearLayout oreosquircletrim = mView.findViewById(R.id.QsTileStyleSquircletrimOreo);
 
         if (ThemeAccentUtils.isUsingQsTileStyles(mOverlayManager, mCurrentUserId, 1 )) {
+            setAlphaForAll(res);
             square.setAlpha((float) 1.0);
-            roundedsquare.setAlpha(mLayoutOpacity);
-            squircle.setAlpha(mLayoutOpacity);
-            teardrop.setAlpha(mLayoutOpacity);
         } else if (ThemeAccentUtils.isUsingQsTileStyles(mOverlayManager, mCurrentUserId, 2 )) {
-            square.setAlpha(mLayoutOpacity);
+            setAlphaForAll(res);
             roundedsquare.setAlpha((float) 1.0);
-            squircle.setAlpha(mLayoutOpacity);
-            teardrop.setAlpha(mLayoutOpacity);
         } else if (ThemeAccentUtils.isUsingQsTileStyles(mOverlayManager, mCurrentUserId, 3 )) {
-            square.setAlpha(mLayoutOpacity);
-            roundedsquare.setAlpha(mLayoutOpacity);
+            setAlphaForAll(res);
             squircle.setAlpha((float) 1.0);
-            teardrop.setAlpha(mLayoutOpacity);
         } else if (ThemeAccentUtils.isUsingQsTileStyles(mOverlayManager, mCurrentUserId, 4 )) {
-            square.setAlpha(mLayoutOpacity);
-            roundedsquare.setAlpha(mLayoutOpacity);
-            squircle.setAlpha(mLayoutOpacity);
+            setAlphaForAll(res);
             teardrop.setAlpha((float) 1.0);
+        } else if (ThemeAccentUtils.isUsingQsTileStyles(mOverlayManager, mCurrentUserId, 5 )) {
+            setAlphaForAll(res);
+            circlegradient.setAlpha((float) 1.0);
+        } else if (ThemeAccentUtils.isUsingQsTileStyles(mOverlayManager, mCurrentUserId, 6 )) {
+            setAlphaForAll(res);
+            circletrim.setAlpha((float) 1.0);
+        } else if (ThemeAccentUtils.isUsingQsTileStyles(mOverlayManager, mCurrentUserId, 7 )) {
+            setAlphaForAll(res);
+            dottedcircle.setAlpha((float) 1.0);
+        } else if (ThemeAccentUtils.isUsingQsTileStyles(mOverlayManager, mCurrentUserId, 8 )) {
+            setAlphaForAll(res);
+            dualtonecircle.setAlpha((float) 1.0);
+        } else if (ThemeAccentUtils.isUsingQsTileStyles(mOverlayManager, mCurrentUserId, 9 )) {
+            setAlphaForAll(res);
+            dualtonecircletrim.setAlpha((float) 1.0);
+        } else if (ThemeAccentUtils.isUsingQsTileStyles(mOverlayManager, mCurrentUserId, 10 )) {
+            setAlphaForAll(res);
+            mountain.setAlpha((float) 1.0);
+        } else if (ThemeAccentUtils.isUsingQsTileStyles(mOverlayManager, mCurrentUserId, 11 )) {
+            setAlphaForAll(res);
+            ninja.setAlpha((float) 1.0);
+        } else if (ThemeAccentUtils.isUsingQsTileStyles(mOverlayManager, mCurrentUserId, 12 )) {
+            setAlphaForAll(res);
+            pokesign.setAlpha((float) 1.0);
+        } else if (ThemeAccentUtils.isUsingQsTileStyles(mOverlayManager, mCurrentUserId, 13 )) {
+            setAlphaForAll(res);
+            wavey.setAlpha((float) 1.0);
+        } else if (ThemeAccentUtils.isUsingQsTileStyles(mOverlayManager, mCurrentUserId, 14 )) {
+            setAlphaForAll(res);
+            squircletrim.setAlpha((float) 1.0);
+        } else if (ThemeAccentUtils.isUsingQsTileStyles(mOverlayManager, mCurrentUserId, 15 )) {
+            setAlphaForAll(res);
+            cookie.setAlpha((float) 1.0);
+        } else if (ThemeAccentUtils.isUsingQsTileStyles(mOverlayManager, mCurrentUserId, 16 )) {
+            setAlphaForAll(res);
+            oreo.setAlpha((float) 1.0);
+        } else if (ThemeAccentUtils.isUsingQsTileStyles(mOverlayManager, mCurrentUserId, 17 )) {
+            setAlphaForAll(res);
+            oreocircletrim.setAlpha((float) 1.0);
+        } else if (ThemeAccentUtils.isUsingQsTileStyles(mOverlayManager, mCurrentUserId, 18 )) {
+            setAlphaForAll(res);
+            oreosquircletrim.setAlpha((float) 1.0);
         } else {
             square.setAlpha((float) 1.0);
             roundedsquare.setAlpha((float) 1.0);
             squircle.setAlpha((float) 1.0);
             teardrop.setAlpha((float) 1.0);
+            circlegradient.setAlpha((float) 1.0);
+            circletrim.setAlpha((float) 1.0);
+            dottedcircle.setAlpha((float) 1.0);
+            dualtonecircle.setAlpha((float) 1.0);
+            dualtonecircletrim.setAlpha((float) 1.0);
+            mountain.setAlpha((float) 1.0);
+            ninja.setAlpha((float) 1.0);
+            pokesign.setAlpha((float) 1.0);
+            wavey.setAlpha((float) 1.0);
+            squircletrim.setAlpha((float) 1.0);
+            cookie.setAlpha((float) 1.0);
+            oreo.setAlpha((float) 1.0);
+            oreocircletrim.setAlpha((float) 1.0);
+            oreosquircletrim.setAlpha((float) 1.0);
         }
+    }
+
+    private void setAlphaForAll(Resources res) {
+        LinearLayout square = mView.findViewById(R.id.QsTileStyleSquare);
+        LinearLayout roundedsquare = mView.findViewById(R.id.QsTileStyleRoundedSquare);
+        LinearLayout squircle = mView.findViewById(R.id.QsTileStyleSquircle);
+        LinearLayout teardrop = mView.findViewById(R.id.QsTileStyleTearDrop);
+        LinearLayout circlegradient = mView.findViewById(R.id.QsTileStyleCirclegradient);
+        LinearLayout circletrim = mView.findViewById(R.id.QsTileStyleCircletrim);
+        LinearLayout dottedcircle = mView.findViewById(R.id.QsTileStyleDottedcircle);
+        LinearLayout dualtonecircle = mView.findViewById(R.id.QsTileStyleDualtonecircle);
+        LinearLayout dualtonecircletrim = mView.findViewById(R.id.QsTileStyleDualtonecircletrim);
+        LinearLayout mountain = mView.findViewById(R.id.QsTileStyleMountain);
+        LinearLayout ninja = mView.findViewById(R.id.QsTileStyleNinja);
+        LinearLayout pokesign = mView.findViewById(R.id.QsTileStylePokesign);
+        LinearLayout wavey = mView.findViewById(R.id.QsTileStyleWavey);
+        LinearLayout squircletrim = mView.findViewById(R.id.QsTileStyleSquircletrim);
+        LinearLayout cookie = mView.findViewById(R.id.QsTileStyleCookie);
+        LinearLayout oreo = mView.findViewById(R.id.QsTileStyleOreo);
+        LinearLayout oreocircletrim = mView.findViewById(R.id.QsTileStyleCircletrimOreo);
+        LinearLayout oreosquircletrim = mView.findViewById(R.id.QsTileStyleSquircletrimOreo);
+
+        TypedValue typedValue = new TypedValue();
+        res.getValue(R.dimen.qs_styles_layout_opacity, typedValue, true);
+        float mLayoutOpacity = typedValue.getFloat();
+
+        square.setAlpha(mLayoutOpacity);
+        roundedsquare.setAlpha(mLayoutOpacity);
+        squircle.setAlpha(mLayoutOpacity);
+        teardrop.setAlpha(mLayoutOpacity);
+        circlegradient.setAlpha(mLayoutOpacity);
+        circletrim.setAlpha(mLayoutOpacity);
+        dottedcircle.setAlpha(mLayoutOpacity);
+        dualtonecircle.setAlpha(mLayoutOpacity);
+        dualtonecircletrim.setAlpha(mLayoutOpacity);
+        mountain.setAlpha(mLayoutOpacity);
+        ninja.setAlpha(mLayoutOpacity);
+        pokesign.setAlpha(mLayoutOpacity);
+        wavey.setAlpha(mLayoutOpacity);
+        squircletrim.setAlpha(mLayoutOpacity);
+        cookie.setAlpha(mLayoutOpacity);
+        oreo.setAlpha(mLayoutOpacity);
+        oreocircletrim.setAlpha(mLayoutOpacity);
+        oreosquircletrim.setAlpha(mLayoutOpacity);
     }
 }

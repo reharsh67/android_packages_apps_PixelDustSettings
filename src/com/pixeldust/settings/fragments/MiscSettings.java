248d6a1ac73f8759976548a23e263492503e2b39
Pranav Vashi <neobuddy89@gmail.com>
2019-06-30 02:05:26 +0530

    GamingMode: Clean up and refine [3/3]
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    Signed-off-by: spezi77 <spezi7713@gmx.net>

--- src/com/pixeldust/settings/fragments/MiscSettings.java
+++ src/com/pixeldust/settings/fragments/MiscSettings.java
@@ -21,7 +21,7 @@ import java.util.List;
 public class MiscSettings extends SettingsPreferenceFragment
         implements Indexable, Preference.OnPreferenceChangeListener {
 
-    private static final String GAMING_MODE_MASTER_SWITCH = "gaming_mode_master_switch";
+    private static final String GAMING_MODE_ENABLED = "gaming_mode_enabled";
     private SystemSettingMasterSwitchPreference mGamingMode;
 
     @Override
@@ -30,9 +30,9 @@ public class MiscSettings extends SettingsPreferenceFragment
 
         addPreferencesFromResource(R.xml.pixeldust_settings_misc);
 
-        mGamingMode = (SystemSettingMasterSwitchPreference) findPreference(GAMING_MODE_MASTER_SWITCH);
+        mGamingMode = (SystemSettingMasterSwitchPreference) findPreference(GAMING_MODE_ENABLED);
         mGamingMode.setChecked((Settings.System.getInt(getActivity().getContentResolver(),
-                Settings.System.GAMING_MODE_MASTER_SWITCH, 1) == 1));
+                Settings.System.GAMING_MODE_ENABLED, 0) == 1));
         mGamingMode.setOnPreferenceChangeListener(this);
     }
 
@@ -41,7 +41,7 @@ public class MiscSettings extends SettingsPreferenceFragment
         if (preference == mGamingMode) {
             boolean value = (Boolean) newValue;
             Settings.System.putInt(getActivity().getContentResolver(),
-                    Settings.System.GAMING_MODE_MASTER_SWITCH, value ? 1 : 0);
+                    Settings.System.GAMING_MODE_ENABLED, value ? 1 : 0);
             return true;
         }
         return false;

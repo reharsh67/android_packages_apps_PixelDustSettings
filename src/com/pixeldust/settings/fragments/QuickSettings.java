a57d9e3807c796a97a917077ba767e6f56daf60c
spezi77 <spezi7713@gmx.net>
2020-06-01 13:46:52 +0200

    QS Settings: Brightness modes should be mutually exclusive. Also improve the QS footer brightness slider strings
    Signed-off-by: spezi77 <spezi7713@gmx.net>

--- src/com/pixeldust/settings/fragments/QuickSettings.java
+++ src/com/pixeldust/settings/fragments/QuickSettings.java
@@ -17,6 +17,7 @@ package com.pixeldust.settings.fragments;
 
 import android.app.Activity;
 import android.content.Context;
+import android.content.ContentResolver;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.UserHandle;
@@ -30,20 +31,27 @@ import androidx.preference.PreferenceCategory;
 import androidx.preference.PreferenceScreen;
 import androidx.preference.Preference.OnPreferenceChangeListener;
 
+import com.android.internal.logging.nano.MetricsProto;
 import com.android.settings.R;
 import com.android.settings.search.BaseSearchIndexProvider;
 import com.android.settings.search.Indexable;
 import com.android.settings.SettingsPreferenceFragment;
 import com.android.settingslib.search.SearchIndexable;
 
-import com.android.internal.logging.nano.MetricsProto;
+import com.pixeldust.settings.preferences.SystemSettingSwitchPreference;
 
 import java.util.ArrayList;
 import java.util.List;
 
 @SearchIndexable
 public class QuickSettings extends SettingsPreferenceFragment implements
-        Indexable {
+        OnPreferenceChangeListener, Indexable {
+
+    private static final String QS_BRIGHTNESS_SLIDER_FOOTER = "qs_brightness_slider_footer";
+    private static final String QS_SHOW_BRIGHTNESS = "qs_show_brightness";
+
+    private SystemSettingSwitchPreference mFooterSlider;
+    private SystemSettingSwitchPreference mQSSlider;
 
     @Override
     public void onCreate(Bundle icicle) {
@@ -51,6 +59,55 @@ public class QuickSettings extends SettingsPreferenceFragment implements
         addPreferencesFromResource(R.xml.pixeldust_settings_quicksettings);
         PreferenceScreen prefSet = getPreferenceScreen();
 
+        boolean isFooterSlider = Settings.System.getIntForUser(getActivity().getContentResolver(),
+                QS_BRIGHTNESS_SLIDER_FOOTER, 0, UserHandle.USER_CURRENT) != 0;
+        mFooterSlider =
+                (SystemSettingSwitchPreference) findPreference(QS_BRIGHTNESS_SLIDER_FOOTER);
+        mFooterSlider.setChecked(isFooterSlider);
+        mFooterSlider.setOnPreferenceChangeListener(this);
+
+        mQSSlider =
+                (SystemSettingSwitchPreference) findPreference(QS_SHOW_BRIGHTNESS);
+        mQSSlider.setOnPreferenceChangeListener(this);
+
+        updateDependencies(mFooterSlider, isFooterSlider);
+    }
+
+    private void updateDependencies(Preference updatedPreference, Boolean newValue) {
+        if (updatedPreference == mFooterSlider) {
+            mFooterSlider.setChecked(newValue);
+            if (newValue) mQSSlider.setChecked(false);
+        } else if (updatedPreference == mQSSlider) {
+            mQSSlider.setChecked(newValue);
+            if (newValue) mFooterSlider.setChecked(false);
+        }
+    }
+
+    @Override
+    public boolean onPreferenceChange(Preference preference, Object objValue) {
+        ContentResolver resolver = getActivity().getContentResolver();
+        if (preference == mFooterSlider) {
+            boolean value = (Boolean) objValue;
+            Settings.System.putIntForUser(getActivity().getContentResolver(),
+                    QS_BRIGHTNESS_SLIDER_FOOTER, value ? 1 : 0, UserHandle.USER_CURRENT);
+            if (value) {
+                Settings.System.putIntForUser(getActivity().getContentResolver(),
+                        QS_SHOW_BRIGHTNESS, 0, UserHandle.USER_CURRENT);
+            }
+            updateDependencies(preference, value);
+            return true;
+        } else if (preference == mQSSlider) {
+            boolean value = (Boolean) objValue;
+            Settings.System.putIntForUser(getActivity().getContentResolver(),
+                    QS_SHOW_BRIGHTNESS, value ? 1 : 0, UserHandle.USER_CURRENT);
+            if (value) {
+                Settings.System.putIntForUser(getActivity().getContentResolver(),
+                        QS_BRIGHTNESS_SLIDER_FOOTER, 0, UserHandle.USER_CURRENT);
+            }
+            updateDependencies(preference, value);
+            return true;
+        }
+        return false;
     }
 
     @Override
